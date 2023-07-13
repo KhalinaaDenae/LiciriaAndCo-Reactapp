eadeb47d33c2914b995c96542a6a7746a909da52
Khalinadenae <129796284+KhalinaaDenae@users.noreply.github.com>
2023-07-13 10:33:54 -0400

    removed changes

--- my-app/src/components/Products.jsx
+++ my-app/src/components/Products.jsx
@@ -3,52 +3,41 @@ import './styles/products.css';
 import axios from 'axios';
 import imageUrls from '../data/imageUrls';
 
+
 export const Products = () => {
   const [products, setProducts] = useState([]);
-  const [categoryFilter, setCategoryFilter] = useState('all');
-  const categories = ['all', 'necklaces', 'earrings', 'bracelets'];
-
-  const getProducts = async (category) => {
-    try {
-      const response = await axios.get(`http://localhost:7080/products/${category}`);
-      const productsWithUrls = response.data.map((product) => ({
-        ...product,
-        imageUrl: imageUrls[product.id],
-      }));
-      return productsWithUrls;
-    } catch (error) {
-      console.error(error);
-      return [];
-    }
-  };
+  const [categoryFilter, setCategoryFilter] = useState("all")
+  const categories = ["necklaces", "earrings", "bracelets"]
 
   useEffect(() => {
-    const fetchData = async () => {
-      const productsWithUrls = await getProducts(categoryFilter);
-      setProducts(productsWithUrls);
-    };
-
-    fetchData();
-  }, [categoryFilter]);
+    axios
+      .get('http://localhost:2040/products')
+      .then((response) => {
+        const productsWithUrls = response.data.map((product) => ({
+          ...product,
+          imageUrl: imageUrls[product.id],
+        }));
+        setProducts(productsWithUrls);
+      })
+      .catch((error) => console.error(error));
+  }, []);
 
   return (
     <div className='shop'>
       <h1 className='header'>Products</h1>
       <div className='product-categories'>
-        {categories.map((category) => (
-          <h4 key={category} onClick={() => setCategoryFilter(category)}>
-            {category}
-          </h4>
-        ))}
+
+      {categories.map((category) => <h4> {category}</h4>)}
+
       </div>
-      <div className='product-list'>
+           <div className='product-list'>
         {products.map((product) => (
           <div className='card' key={product.id}>
             <img className='product-image' src={product.imageUrl} alt='product' />
             <h3 className='product-name'>{product.name}</h3>
             <p className='product-description'>{product.description}</p>
             <p className='product-price'>${product.price}</p>
-            <button className='add-to-bag-button'>View Details</button>
+            <button className='add-to-bag-button'> View Details </button>
           </div>
         ))}
       </div>
