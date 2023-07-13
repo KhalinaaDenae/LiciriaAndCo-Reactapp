867754a805101aa438cbc57ea2b1558be0e4da29
Khalinadenae <129796284+KhalinaaDenae@users.noreply.github.com>
2023-07-13 14:21:46 -0400

    function to filter categories

--- my-app/src/components/Products.jsx
+++ my-app/src/components/Products.jsx
@@ -1,72 +1,83 @@
-import React, { useEffect, useState } from 'react';
-import './styles/products.css';
-import axios from 'axios';
-import imageUrls from '../data/imageUrls';
+import React, { useEffect, useState } from "react";
+import "./styles/products.css";
+import axios from "axios";
+import imageUrls from "../data/imageUrls";
 
-//   <div className='product-categories'>
-
-// {categories.map((category) => <h4> {category}</h4>)}
-
-// </div>
-
-//  const ecommerceProducts = products
-
-//{ecommerceProducts.map((products) => {
-  //console.log(products.category)
-  //let categories = products.category
- // return categories
-//})}
-
-//useEffect(() => {
-  //let filteredProducts = categories ;
-
-  // if (category !== "all") {
-    //filteredProducts = filteredProducts.filter(product => product.category === category)
-  //}
-  //setProducts(filteredProducts)
-// })
 export const Products = () => {
   const [products, setProducts] = useState([]);
-  const [category, setCategory] = useState("all")
-  const categories = ["necklaces", "earrings", "bracelets"]
-
+  const [initialProducts, setInitialProducts] = useState([]);
+  const categories = ["necklace", "earring", "bracelet", "all"];
 
   useEffect(() => {
-    axios
-      .get('http://localhost:2040/products')
-      .then((response) => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("http://localhost:7080/products");
         const productsWithUrls = response.data.map((product) => ({
           ...product,
           imageUrl: imageUrls[product.id],
         }));
         setProducts(productsWithUrls);
-        console.log(products)
-      })
-      .catch((error) => console.error(error));
+        setInitialProducts(productsWithUrls);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
   }, []);
 
+  const filterCategory = async (category) => {
+    if (category === "all") {
+      setProducts(initialProducts);
+    }
 
-  
+    try {
+      const response = await axios.get(
+        `http://localhost:7080/products/${category}`,
+        { params: { category } }
+      );
+      const filteredProductsWithUrls = response.data.map((product) => ({
+        ...product,
+        imageUrl: imageUrls[product.id],
+      }));
+      setProducts(filteredProductsWithUrls);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-
-
-
+  const handleCategoryClick = (category) => {
+    //setSelectedCategory(category);
+    filterCategory(category);
+  };
 
   return (
-    <div className='shop'>
-      <h1 className='header'>Products</h1>
-   
-           <div className='product-list'>
+    <div className="shop">
+      <h1 className="header">Products</h1>
+      <div className="product-categories">
+        {categories.map((category) => (
+          <h4 key={category} onClick={() => handleCategoryClick(category)}>
+            {category}
+          </h4>
+        ))}
+      </div>
+      <div className="product-list">
         {products.map((product) => (
-          <div className='card' key={product.id}>
-            <img className='product-image' src={product.imageUrl} alt='product' />
-            <h3 className='product-name'>{product.name}</h3>
-            <p className='product-description'>{product.description}</p>
-            <p className='product-price'>${product.price}</p>
-            <button className='add-to-bag-button'> View Details </button>
+          <div className="card" key={product.id}>
+            <img
+              className="product-image"
+              src={product.imageUrl}
+              alt="product"
+            />
+            <h3 className="product-name">{product.name}</h3>
+            <p className="product-description">{product.description}</p>
+            <p className="product-price">${product.price}</p>
+            <button className="add-to-bag-button">View Details</button>
           </div>
         ))}
       </div>
     </div>
   );
 };
+
+// products .sort
