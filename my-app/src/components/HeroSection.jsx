f7a2e2ae7af884526821bc63e21ebb2453da4007
Khalinadenae <129796284+KhalinaaDenae@users.noreply.github.com>
2023-07-13 23:06:35 -0400

    styled hero section

--- my-app/src/components/HeroSection.jsx
+++ my-app/src/components/HeroSection.jsx
@@ -1,22 +1,18 @@
-import React from 'react';
+import React from "react";
 
 const HeroSection = () => {
-    return (
-      <div className="hero-section">
-          <img 
-          src={require('./homeimg.jpeg')} 
-          alt="logo" 
-        />
-        <div className="hero-content">
-          <p>A inspirational jewelry brand symbolising elegance & self-belief <br /> accentuating unique traits that make you YOU. 
-</p>
-<br />
-<h3 className='shop-cta'> SHOP NOW </h3>
-        
-        </div>
+  return (
+    <div className="hero-section">
+      <img className="heroimg" src={require("./homeimg.jpeg")} alt="logo" />
+      <div className="hero-content">
+        <p>
+          A inspirational jewelry brand symbolising elegance & self-belief{" "}
+          accentuating unique traits that make you YOU.
+        </p>
+        <br />
       </div>
-    );
-  };
-  
-  export default HeroSection;
-  
+    </div>
+  );
+};
+
+export default HeroSection;
