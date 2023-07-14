04a17685c8ecaf08beb6e36c575b08c2094f1aea
Khalinadenae <129796284+KhalinaaDenae@users.noreply.github.com>
2023-07-14 19:24:20 -0400

    styled social media section

--- my-app/src/components/SocialMedia.jsx
+++ my-app/src/components/SocialMedia.jsx
@@ -2,18 +2,12 @@ import React from "react";
 
 const SocialMedia = () => {
   const imagePaths = [
-    require("../images/image1.png"),
     require("../images/image2.png"),
-    require("../images/image3.png"),
     require("../images/image4.png"),
-    require("../images/image5.png"),
     require("../images/image6.png"),
     require("../images/image7.png"),
-    require("../images/image8.png"),
     require("../images/image9.png"),
-    require("../images/image10.png"),
     require("../images/image11.png"),
-    require("../images/image12.png"),
   ];
 
   return (
