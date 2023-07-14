e26f516fa57c1e513025691a61d0158ab02e17c0
Khalinadenae <129796284+KhalinaaDenae@users.noreply.github.com>
2023-07-13 23:29:46 -0400

    styled categories section

--- my-app/src/components/Categories.jsx
+++ my-app/src/components/Categories.jsx
@@ -1,48 +1,43 @@
-import React from 'react';
+import React from "react";
 
 const Categories = () => {
-    const categoryData = [
-      {
-        name: 'Earrings',
-        image: require('../images/category1.png')
-      },
-      {
-        name: 'Rings',
-        image: require('../images/category2.png')
-      },
-      {
-        name: 'Bracelets',
-        image: require('../images/category3.png')
-      },
-      {
-        name: 'Anklets',
-        image: require('../images/category4.png')
-      },
-      {
-        name: 'Necklaces',
-        image: require('../images/category5.png')
-      }
-    ];
-  
-    return (
-        <section className="categories">
-        <div className="categories-container">
-            <h1>
-                Discover our everyday pieces 
-            </h1>
-          <div className="category-image-grid">
-            {categoryData.map((category, index) => (
-              <div className="image-container" key={index}>
-                <img src={category.image} alt={`Category ${index + 1}`} />
-                <div className="overlay">
-                  <h4>{category.name}</h4>
-                </div>
+  const categoryData = [
+    {
+      name: "Earrings",
+      image: require("../images/category1.png"),
+    },
+    {
+      name: "Rings",
+      image: require("../images/category2.png"),
+    },
+    {
+      name: "Bracelets",
+      image: require("../images/category3.png"),
+    },
+    {
+      name: "Necklaces",
+      image: require("../images/category5.png"),
+    },
+  ];
+
+  return (
+    <section className="categories">
+      <div className="categories-container">
+        <h6 className="mute"> Get ready to sparkle</h6>
+        <h1 className="categories-header">Discover our collections</h1>
+        <div className="category-image-grid">
+          {categoryData.map((category, index) => (
+            <div className="image-container" key={index}>
+              <img src={category.image} alt={`Category ${index + 1}`} />
+              <div className="overlay">
+                <h4> Shop Now </h4>
               </div>
-            ))}
-          </div>
+              <div className="category-name">{category.name}</div>
+            </div>
+          ))}
         </div>
-      </section>
-  
-    );
-  };
+      </div>
+    </section>
+  );
+};
 export default Categories;
