1c1222435912d5eb482b2b8d36ee348edb6b5c25
Khalinadenae <129796284+KhalinaaDenae@users.noreply.github.com>
2023-07-13 23:48:14 -0400

    styled slider component

--- my-app/src/components/BestSellers.jsx
+++ my-app/src/components/BestSellers.jsx
@@ -1,74 +1,82 @@
-import React from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
-import 'swiper/css';
-import 'swiper/css/scrollbar';
-import bestSellersslideOne from '../data/bestSellersData';
-import bestSellersslideTwo from '../data/slideTwo';
-import bestSellersslideThree from '../data/slideThree';
-
-//imported sqiper API for swiper component 
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/scrollbar";
+import bestSellersslideOne from "../data/bestSellersData";
+import bestSellersslideTwo from "../data/slideTwo";
+import bestSellersslideThree from "../data/slideThree";
 
 const BestSellers = () => {
   return (
     <section className="best-sellers">
-            <h2 className="section-title">Best Sellers</h2>
-      <Swiper
-      // install Swiper modules
-      modules={[Navigation, Pagination, Scrollbar, A11y]}
-      spaceBetween={10}
-      slidesPerView={1}
-      navigation
-      pagination={{ clickable: true }}
-      scrollbar={{ draggable: true }}
-      onSwiper={(swiper) => console.log(swiper)}
-      onSlideChange={() => console.log('slide change')}
-    >
-      <SwiperSlide>
       <div className="best-sellers-container">
-        <div className="image-grid">
-          {bestSellersslideOne.map((item, index) => (
-            <div className="best-seller-item" key={index}>
-              <img className='bestSellerImg' 
-              src={item.image} alt={`Best Seller ${index + 1}`} />
-              <h3 className="item-title">{item.name}</h3>
-              <p className="item-price">{item.price}</p>
-            </div>
-          ))}
+        <div className="slider-container">
+          <div className="header">
+            <h2 className="section-title">SHOP NEW IN</h2>
+            <p className="new-in-text">
+              Stylish, sleek and glam pieces to elevate your personal style
+            </p>
+          </div>
+          <Swiper
+            className="swiper"
+            modules={[Navigation, Pagination, Scrollbar, A11y]}
+            spaceBetween={10}
+            slidesPerView={1}
+            navigation
+            pagination={{ clickable: true }}
+            scrollbar={{ draggable: true }}
+            onSwiper={(swiper) => console.log(swiper)}
+            onSlideChange={() => console.log("slide change")}
+          >
+            <SwiperSlide>
+              <div className="image-grid">
+                {bestSellersslideOne.map((item, index) => (
+                  <div className="best-seller-item" key={index}>
+                    <img
+                      className="bestSellerImg"
+                      src={item.image}
+                      alt={`Best Seller ${index + 1}`}
+                    />
+                    <h3 className="item-title">{item.name}</h3>
+                    <p className="item-price">{item.price}</p>
+                  </div>
+                ))}
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="image-grid">
+                {bestSellersslideTwo.map((item, index) => (
+                  <div className="best-seller-item" key={index}>
+                    <img
+                      className="bestSellerImg"
+                      src={item.image}
+                      alt={`Best Seller ${index + 1}`}
+                    />
+                    <h3 className="item-title">{item.name}</h3>
+                    <p className="item-price">{item.price}</p>
+                  </div>
+                ))}
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="image-grid">
+                {bestSellersslideThree.map((item, index) => (
+                  <div className="best-seller-item" key={index}>
+                    <img
+                      className="bestSellerImg"
+                      src={item.image}
+                      alt={`Best Seller ${index + 1}`}
+                    />
+                    <h3 className="item-title">{item.name}</h3>
+                    <p className="item-price">{item.price}</p>
+                  </div>
+                ))}
+              </div>
+            </SwiperSlide>
+          </Swiper>
         </div>
       </div>
-      </SwiperSlide>
-      <SwiperSlide>
-      <div className="best-sellers-container">
-
-        <div className="image-grid">
-          {bestSellersslideTwo.map((item, index) => (
-            <div className="best-seller-item" key={index}>
-              <img className='bestSellerImg' 
-              src={item.image} alt={`Best Seller ${index + 1}`} />
-              <h3 className="item-title">{item.name}</h3>
-              <p className="item-price">{item.price}</p>
-            </div>
-          ))}
-        </div>
-      </div>
-      </SwiperSlide>
-      <SwiperSlide>
-      <div className="best-sellers-container">
-  
-        <div className="image-grid">
-          {bestSellersslideThree.map((item, index) => (
-            <div className="best-seller-item" key={index}>
-              <img className='bestSellerImg' 
-              src={item.image} alt={`Best Seller ${index + 1}`} />
-              <h3 className="item-title">{item.name}</h3>
-              <p className="item-price">{item.price}</p>
-            </div>
-          ))}
-        </div>
-      </div>
-      </SwiperSlide>
-    </Swiper>
     </section>
   );
 };
